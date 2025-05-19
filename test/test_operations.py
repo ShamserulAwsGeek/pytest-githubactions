1878f8513db82f2e8caaec9fc27dbe45b8bf3cde
ShamserulAwsGeek <shamserul786@gmail.com>
2025-05-19 12:26:10 +0530

    python path changed

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,5 +1,11 @@
 from src.math_operations import add,sub
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from src.math_operations import add, sub
+
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
