9b2e43faa1e495125f59a8b1988be6e39f93bdfb
ShamserulAwsGeek <shamserul786@gmail.com>
2025-05-20 12:02:30 +0530

    added test file for workflow

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,4 +1,4 @@
-from src.math_operations import add,sub
+#from src.math_operations import add,sub
 
 # import sys
 # import os
@@ -6,12 +6,30 @@ from src.math_operations import add,sub
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 # from src.math_operations import add, sub
 
+# def test_add():
+#     assert add(2,3)==5
+#     assert add(-1,1)==0
+    
+# def test_sub():
+#     assert sub(5,3)==2
+#     assert sub(4,3)==1
+#     assert sub(3,3)==0
+#     assert sub(2,3)==-1
+
+
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from src.math_operations import add, sub
+
 def test_add():
-    assert add(2,3)==5
-    assert add(-1,1)==0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
     
 def test_sub():
-    assert sub(5,3)==2
-    assert sub(4,3)==1
-    assert sub(3,3)==0
-    assert sub(2,3)==-1
+    assert sub(5, 3) == 2
+    assert sub(4, 3) == 1
+    assert sub(3, 3) == 0
+    assert sub(2, 3) == -1
